fe0cf9ef8348c7dde6659bd1ab15ce94b3166bd1
Taylor Sun <Taylor_Sun@epam.com>
2020-03-23 16:38:58 +0800

    [demos: link] FE link

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
-export default () => <div>Welcome to next.js!</div>
+import 'isomorphic-unfetch';
+import Link from 'next/link';
+
+const Page = ({ stars }) =>
+    <div>
+        <h1>Welcome to next.js!</h1>
+        <div>
+            Next stars: {stars}⭐️
+        </div>
+        <br />
+        <div>
+            Click{' '}
+            <Link href="/about">
+                <a>here</a>
+            </Link>{' '}
+            to read more
+        </div>
+    </div>
+;
+
+Page.getInitialProps = async ({ req }) => {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+};
+
+export default Page;
