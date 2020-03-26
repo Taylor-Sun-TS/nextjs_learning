76023e6a07ef4684cf0dd2fd3613e3fd0faeb024
Taylor Sun <Taylor_Sun@epam.com>
2020-03-26 17:42:44 +0800

    [bugfix: typo error] fix typo error and modify some deprecated features

--- pages/_error.js
+++ pages/_error.js
@@ -1,30 +1,26 @@
 function Error({ statusCode }) {
-    return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
+}
 
   // Error.getInitialProps = ({ res, err }) => {
   //   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
   //   return { statusCode }
   // }
 
-  // export async function getServerSideProps(context) {
-  export async function getServerSideProps({ res, err }) {
-    // return {
-    //   props: {}, // will be passed to the page component as props
-    // }
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+export async function getServerSideProps({ res }) {
+  const statusCode = res ? res.statusCode : 404
     return {
       props:
       {
         statusCode
       }
-    }
   }
+}
 
   export default Error;
