02b904de7fd76bfb6c8a8392478d0dfb86640c9e
Taylor Sun <Taylor_Sun@epam.com>
2020-03-26 17:04:50 +0800

    [demos: staticGeneration_ssr] replace getInitialProps with getStaticProps,getServerSideProps

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,15 @@ import './styles.css';
 import Header from './components/header.js'
 
 class MyApp extends App {
-    static async getInitialProps({ Component, router, ctx }) {
-        let pageProps = { };
+    // static async getInitialProps({ Component, router, ctx }) {
+    //     let pageProps = { };
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
+    //     if (Component.getInitialProps) {
+    //         pageProps = await Component.getInitialProps(ctx);
+    //     }
 
-        return { pageProps };
-    };
-
+    //     return { pageProps };
+    // };
 
     render() {
         const { Component, pageProps } = this.props;
@@ -23,6 +22,16 @@ class MyApp extends App {
             <Component {...pageProps} />
         </container>)
     }
+}
+
+export async function getStaticProps({ Component, router, ctx }) {
+    let pageProps = { };
+
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
 }
 
 export default MyApp;
--- pages/components/helloBundle.js
+++ pages/components/helloBundle.js
@@ -1,24 +1,43 @@
 import dynamic from 'next/dynamic';
 
-const HelloBundle = dynamic({
-    modules: () => {
-        const components = {
-            Hello1: import('./hello'),
-            Hello2: import('./hello')
-        }
+// deprecated feature
+// const HelloBundle = dynamic({
+//     modules: () => {
+//         const components = {
+//             Hello1: import('./hello'),
+//             Hello2: import('./hello')
+//         }
 
-        return components
-    },
-    render: (props, { Hello1, Hello2 }) =>(
-        <div>
-            <h1>
-                {props.title}
-            </h1>
-            <Hello1 name={JSON.stringify(props)} />
-            <Hello1 name={props.name1} />
-            <Hello2 name={props.name2} />
-        </div>
-    )
-});
+//         return components
+//     },
+//     render: (props, { Hello1, Hello2 }) =>(
+//         <div>
+//             <h1>
+//                 {props.title}
+//             </h1>
+//             <Hello1 name={JSON.stringify(props)} />
+//             <Hello1 name={props.name1} />
+//             <Hello2 name={props.name2} />
+//         </div>
+//     )
+// });
 
-export default ({hello1, hello2}) => <HelloBundle title="Dynamic Bundle" name1={hello1} name2={hello2} />
+const Hello1 = dynamic(() => import('./hello'))
+const Hello2 = dynamic(() => import('./hello'))
+
+function HelloBundle(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <Hello1 name={JSON.stringify(props)} />
+      <Hello1 name={props.name1} />
+      <Hello2 name={props.name2} />
+    </div>
+  )
+}
+
+function DynamicBundle({hello1, hello2}) {
+  return <HelloBundle title="Dynamic Bundle" name1={hello1} name2={hello2} />
+}
+
+export default DynamicBundle;
